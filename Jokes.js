e1d9082df323e9c525e09f9268dd39647673c98b
weirdguy01 <107738377+weirdguy01@users.noreply.github.com>
2023-11-03 18:08:51 -0400

    1st commit
    jokes project, first commit

---
+++ Jokes.js
@@ -0,0 +1,64 @@
+const element = document.getElementById("jokeButton");
+element.addEventListener("click", Jokes);
+
+// Array containing some light-hearted jokes with authors
+const jokesArray = [
+    {
+        "joke": "Why don't scientists trust atoms? Because they make up everything!",
+       
+        "author": "Anonymous"
+    },
+    {
+        "joke": "Why did the scarecrow win an award? Because he was outstanding in his field!",
+        
+        "author": "John Doe"
+    },
+    {
+        "joke": "What do you call fake spaghetti? An Impasta!",
+        
+        "author": "Jane Smith"
+    },
+    {
+        "joke": "Why was the math book sad? Because it had too many problems.",
+        
+        "author": "Math Enthusiast"
+    },
+    {
+        "joke": "Why did the golfer bring two pairs of pants? In case he got a hole in one.",
+        
+        "author": "Golf Lover"
+    },
+    {
+        "joke": "Why do we never tell secrets on a farm? Because the potatoes have eyes and the corn has ears.",
+       
+        "author": "Farm Fanatic"
+    },
+    {
+        "joke": "What do you get when you cross a vampire and a snowman? Frostbite.",
+        
+        "author": "Monster Mixer"
+    },
+    {
+        "joke": "How do you organize a space party? You planet.",
+     
+        "author": "Space Enthusiast"
+    },
+    {
+        "joke": "Why did the bicycle fall over? Because it was two-tired!",
+        
+        "author": "Bike Rider"
+    },
+    {
+        "joke": "What do you call a bear with no teeth? A gummy bear!",
+       
+        "author": "Nature Lover"
+    }
+];
+
+function Jokes() {
+    const randomNumber = Math.floor(Math.random() * jokesArray.length);
+    // Display random joke and author based on random number
+    document.getElementById("joke").textContent = jokesArray[randomNumber].joke;
+    document.getElementById("author").textContent = "Author: " + jokesArray[randomNumber].author;
+}
+
